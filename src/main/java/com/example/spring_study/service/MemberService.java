be5a6a071fca8de3ee6d593e13481fe3230b9565
bestdevmgp <pmg3858@icloud.com>
2025-01-28 20:41:17 +0900

    feat: MemberService

---
+++ src/main/java/com/example/spring_study/service/MemberService.java
@@ -0,0 +1,42 @@
+package com.example.spring_study.service;
+
+import com.example.spring_study.domain.Member;
+import com.example.spring_study.repository.MemberRepository;
+import com.example.spring_study.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        //같은 이름이 있는 중복 회원 X
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findyById(memberId);
+    }
+
+}
